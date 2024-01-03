90a68e7fcc6db71b038130cc65d10aad8dc057fc
Burak Bilgili <burak.bilgili@icloud.com>
2024-01-03 17:42:34 +0300

    render projects and loading spinner

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,26 @@ import { useFetchProjects } from '../hooks/fetchProjects';
 const Projects = () => {
   const { isLoading, projects } = useFetchProjects();
 
-  return <div>Projects</div>;
+  if(isLoading){
+    return <section className='projects'>
+      <div className="loading"></div>
+    </section>
+  }
+
+  return <section className='projects'>
+    <div className="title">
+      <h2>Projects</h2>
+      <div className="title-underline"></div>
+      <div className="projects-center">
+        {projects.map(({id, img, url, title})=>(
+          <a href={url} key={id} target='_blank' rel='noreferrer' className='project'>
+            <img src={img} alt={title} className='image' />
+            <h5>{title}</h5>
+          </a>
+        ))}
+      </div>
+    </div>
+  </section>;
 };
 
 export default Projects;
