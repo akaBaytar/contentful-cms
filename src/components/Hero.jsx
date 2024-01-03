dfcf79364bfd24832dbfe7c3298839538b9f4a99
Burak Bilgili <burak.bilgili@icloud.com>
2024-01-03 16:01:42 +0300

    hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,21 @@ import hero from '../../assets/svg/hero.svg'
 
 const Hero = () => {
   return (
-    <div>hero</div>
-  )
+    <section className='hero'>
+      <div className='hero-center'>
+        <div className='hero-title'>
+          <h1>Contentful CMS</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad doloribus corrupti a ipsum eaque cum reiciendis
+            quaerat. Debitis, exercitationem.
+          </p>
+        </div>
+        <div className='image-container'>
+          <img src={hero} alt='Contentful CMS' className='image' />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Hero
